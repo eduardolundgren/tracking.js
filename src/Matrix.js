5931fe2a7b018ac846501e20bdc6c11641afd8ff
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-07-01 02:05:01 -0300

    Removes unused tracking.Matrix.transform method Adds tracking.Matrix.computeIntegralImage method

--- src/Matrix.js
+++ src/Matrix.js
@@ -36,24 +36,57 @@
   };
 
   /**
-   * Loops the pixels array modifying each pixel based on `fn` transformation
-   * function.
-   * @param {array} pixels The pixels in a linear [r,g,b,a,...] array to loop
-   *     through.
+   * Computes the integral image and the integral image squared of the input
+   * pixels. The input pixels will be converted to grayscale using the
+   * following transformation, `~~(r * 0.299 + b * 0.587 + g * 0.114)`. A
+   * summed area table is a data structure and algorithm for quickly and
+   * efficiently generating the sum of values in a rectangular subset of a
+   * grid. In the image processing domain, it is also known as an integral
+   * image.
+   * @param {Uint8ClampedArray} pixels The pixels to loop through.
    * @param {number} width The image width.
    * @param {number} height The image height.
-   * @param {function} fn The transformation function.
-   * @return {array} The transformed pixels.
+   * @return {Array.<Array.<number>>} Array containing in the first position
+   *     the integral image and in the second position the integral image
+   *     squared.
    * @static
    */
-  tracking.Matrix.transform = function(pixels, width, height, fn) {
-    tracking.Matrix.forEach(pixels, width, height, function(r, g, b, a, w) {
-      var pixel = fn.apply(null, arguments);
-      pixels[w] = pixel[0];
-      pixels[w + 1] = pixel[1];
-      pixels[w + 2] = pixel[2];
-      pixels[w + 3] = pixel[3];
-    });
-    return pixels;
+  tracking.Matrix.computeIntergralImage = function(pixels, width, height) {
+    var integralImage = new Int32Array(width * height),
+      integralImageSquare = new Int32Array(width * height),
+      position = 0,
+      pixelSum = 0,
+      pixelSumSquare = 0;
+
+    for (var i = 0; i < height; i++) {
+      for (var j = 0; j < width; j++) {
+        var w = i * width * 4 + j * 4;
+        var pixel = ~~(pixels[w] * 0.299 + pixels[w+1] * 0.587 + pixels[w+2] * 0.114);
+
+        if (i === 0 && j === 0) {
+          pixelSum = pixel;
+          pixelSumSquare = pixel * pixel;
+        } else if (i === 0) {
+          pixelSum = pixel + integralImage[i * width + (j - 1)];
+          pixelSumSquare = pixel * pixel + integralImageSquare[i * width + (j - 1)];
+        } else if (j === 0) {
+          pixelSum = pixel + integralImage[(i - 1) * width + j];
+          pixelSumSquare = pixel * pixel + integralImageSquare[(i - 1) * width + j];
+        } else {
+          pixelSum = pixel + integralImage[i * width + (j - 1)] + integralImage[(i - 1) * width + j] - integralImage[(i - 1) * width + (j - 1)];
+          pixelSumSquare = pixel * pixel + integralImageSquare[i * width + (j - 1)] + integralImageSquare[(i - 1) * width + j] - integralImageSquare[(i - 1) * width + (j - 1)];
+        }
+
+        integralImage[position] = pixelSum;
+        integralImageSquare[position] = pixelSumSquare;
+        position++;
+      }
+    }
+
+    return [
+      integralImage,
+      integralImageSquare
+    ];
   };
+
 }());
