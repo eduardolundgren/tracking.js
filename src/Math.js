c5176d3c113421b98732e9c6740a4cfaa0b11569
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-07-01 02:05:41 -0300

    Adds tracking.Math.intersects method

--- src/Math.js
+++ src/Math.js
@@ -52,4 +52,26 @@
   tracking.Math.uniformRandom = function(a, b) {
     return a + Math.random() * (b - a);
   };
+
+  /**
+   * Tests if a rectangle intersects with another.
+   *
+   *  x0y0 --------       x2y2 --------
+   *      |       |           |       |
+   *      -------- x1y1       -------- x3y3
+   *
+   * @param {number} x0 Horizontal coordinate of P0.
+   * @param {number} y0 Vertical coordinate of P0.
+   * @param {number} x1 Horizontal coordinate of P1.
+   * @param {number} y1 Vertical coordinate of P1.
+   * @param {number} x2 Horizontal coordinate of P2.
+   * @param {number} y2 Vertical coordinate of P2.
+   * @param {number} x3 Horizontal coordinate of P3.
+   * @param {number} y3 Vertical coordinate of P3.
+   * @return {boolean}
+   */
+  tracking.Math.intersectRect = function(x0, y0, x1, y1, x2, y2, x3, y3) {
+    return !(x2 > x1 || x3 < x0 || y2 > y1 || y3 < y0);
+  };
+
 }());
