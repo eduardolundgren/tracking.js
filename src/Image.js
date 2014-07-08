9c931b02660f7e3ee81de6316da359976a9c64ab
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-07-08 10:45:42 -0300

    Renames grayscale image utility and make sure it returns a [p,p,p,a,...] format for consistency

--- src/Image.js
+++ src/Image.js
@@ -12,21 +12,24 @@
    * measured intensity perception of typical trichromat humans, in
    * particular, human vision is most sensitive to green and least sensitive
    * to blue.
-   * @param {Uint8ClampedArray} pixels The pixels in a linear [r,g,b,a,...]
-   *     array.
+   * @param {pixels} pixels The pixels in a linear [r,g,b,a,...] array.
    * @param {number} width The image width.
    * @param {number} height The image height.
-   * @return {Uint8ClampedArray} The grayscale pixels in a linear [p1,p2,...]
-   *     array, where `pn = rn*0.299 + gn*0.587 + bn*0.114`.
+   * @param {Uint8ClampedArray} The grayscale pixels in a linear [p,p,p,a,...]
+   *     array.
    * @static
    */
-  tracking.Image.calculateLumaGrayscale = function(pixels, width, height) {
-    var gray = new Uint8ClampedArray(width * height);
+  tracking.Image.grayscale = function(pixels, width, height) {
+    var gray = new Uint8ClampedArray(width * height * 4);
     var p = 0;
     var w = 0;
     for (var i = 0; i < height; i++) {
       for (var j = 0; j < width; j++) {
-        gray[p++] = pixels[w]*0.299 + pixels[w + 1]*0.587 + pixels[w + 2]*0.114;
+        var value = pixels[w] * 0.299 + pixels[w + 1] * 0.587 + pixels[w + 2] * 0.114;
+        gray[p++] = value;
+        gray[p++] = value;
+        gray[p++] = value;
+        gray[p++] = pixels[w + 3];
         w += 4;
       }
     }
