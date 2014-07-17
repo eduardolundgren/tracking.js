c386c3a2845af7c32cda8e21c87dc66e8e4e2646
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-07-16 23:44:22 -0300

    Adds possibility to re-run a TrackerTask

--- src/trackers/TrackerTask.js
+++ src/trackers/TrackerTask.js
@@ -12,11 +12,6 @@
     }
 
     this.setTracker(tracker);
-
-    this.reemitTrackEvent_ = function(event) {
-      this.emit('track', event);
-    }.bind(this);
-    tracker.on('track', this.reemitTrackEvent_);
   };
 
   tracking.inherits(tracking.TrackerTask, tracking.EventEmitter);
@@ -29,11 +24,36 @@
   tracking.TrackerTask.prototype.tracker_ = null;
 
   /**
+   * Holds if the tracker task is in running.
+   * @type {boolean}
+   * @private
+   */
+  tracking.TrackerTask.prototype.running_ = false;
+
+  /**
    * Gets the tracker instance managed by this task.
    * @return {tracking.Tracker}
    */
   tracking.TrackerTask.prototype.getTracker = function() {
     return this.tracker_;
+  };
+
+  /**
+   * Returns true if the tracker task is in running, false otherwise.
+   * @return {boolean}
+   * @private
+   */
+  tracking.TrackerTask.prototype.inRunning = function() {
+    return this.running_;
+  };
+
+  /**
+   * Sets if the tracker task is in running.
+   * @param {boolean} running
+   * @private
+   */
+  tracking.TrackerTask.prototype.setRunning = function(running) {
+    this.running_ = running;
   };
 
   /**
@@ -45,11 +65,39 @@
   };
 
   /**
+   * Emits a `run` event on the tracker task for the implementers to run any
+   * child action, e.g. `requestAnimationFrame`.
+   * @return {object} Returns itself, so calls can be chained.
+   */
+  tracking.TrackerTask.prototype.run = function() {
+    var self = this;
+
+    if (this.inRunning()) {
+      return;
+    }
+
+    this.setRunning(true);
+    this.reemitTrackEvent_ = function(event) {
+      self.emit('track', event);
+    };
+    this.tracker_.on('track', this.reemitTrackEvent_);
+    this.emit('run');
+    return this;
+  };
+
+  /**
    * Emits a `stop` event on the tracker task for the implementers to stop any
-   * child action being done, such as `requestAnimationFrame`.
+   * child action being done, e.g. `requestAnimationFrame`.
+   * @return {object} Returns itself, so calls can be chained.
    */
   tracking.TrackerTask.prototype.stop = function() {
+    if (!this.inRunning()) {
+      return;
+    }
+
+    this.setRunning(false);
     this.emit('stop');
     this.tracker_.removeListener('track', this.reemitTrackEvent_);
+    return this;
   };
 }());
