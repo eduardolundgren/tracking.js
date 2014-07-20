92320fded366c630a36c853242031b961fc0d4dc
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-07-20 17:03:58 -0300

    Do not test performance descriptors on match

--- test/perf/Brief.js
+++ test/perf/Brief.js
@@ -7,20 +7,24 @@ var image1Gray;
 var image2Gray;
 var imageHeight = 192;
 var imageWidth = 256;
+var descriptors1;
+var descriptors2;
 
 module.exports = {
   setUp: function(done) {
     PNG.decode('test/assets/box1.png', function(pixels1) {
       image1Gray = tracking.Image.grayscale(pixels1, imageWidth, imageHeight);
       corners1 = tracking.Fast.findCorners(image1Gray, imageWidth, imageHeight);
+      descriptors1 = tracking.Brief.getDescriptors(image1Gray, imageWidth, corners1);
 
       PNG.decode('test/assets/box2.png', function(pixels2) {
         image2Gray = tracking.Image.grayscale(pixels2, imageWidth, imageHeight);
         corners2 = tracking.Fast.findCorners(image2Gray, imageWidth, imageHeight);
-
+        descriptors2 = tracking.Brief.getDescriptors(image2Gray, imageWidth, corners1);
         done();
       });
     });
+
   },
 
   testGetDescriptors: function() {
@@ -28,9 +32,6 @@ module.exports = {
   },
 
   testFindMatchingCorners: function() {
-    var descriptors1 = tracking.Brief.getDescriptors(image1Gray, imageWidth, corners1);
-    var descriptors2 = tracking.Brief.getDescriptors(image2Gray, imageWidth, corners2);
-
     tracking.Brief.match(corners1, descriptors1, corners2, descriptors2);
   }
 };
