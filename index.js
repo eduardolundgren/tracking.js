5782b6df81fb58fc78d969117f23efc36272c87d
JoshuaTang <1240604020@qq.com>
2017-07-19 21:26:26 +0800

    add support of CommonJS modules

---
+++ index.js
@@ -0,0 +1,11 @@
+var tracking = require('./build/tracking.js');
+var eye = require('./build/data/eye.js');
+var face = require('./build/data/face.js');
+var mouth = require('./build/data/mouth.js');
+
+module.exports = {
+  tracking: tracking,
+  eye: eye,
+  face: face,
+  mouth: mouth
+};
